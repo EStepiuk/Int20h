eedc9d7faf2f7332c5a6e2f4e4b8fb4a013747ef
Yevhen Stepiuk <stepyuk.android@gmail.com>
2016-02-27 19:32:55 +0200

    merged

--- app/src/main/java/com/gunsnrocket/int20h/adapters/CategoryAdapter.java
+++ app/src/main/java/com/gunsnrocket/int20h/adapters/CategoryAdapter.java
@@ -1,19 +1,37 @@
 package com.gunsnrocket.int20h.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.gunsnrocket.int20h.R;
+import com.gunsnrocket.int20h.models.Category;
+
+import java.util.List;
 
 /**
  * Created by dnt on 2/27/16.
  */
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder>{
 
+    private Context context;
+
+    private List<Category> list;
+
+    public CategoryAdapter(Context context, List<Category> list) {
+        this.context = context;
+        this.list = list;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item_category, parent, false));
     }
 
     @Override
@@ -23,7 +41,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 10;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/com/gunsnrocket/int20h/fragments/CategoriesFragment.java
+++ app/src/main/java/com/gunsnrocket/int20h/fragments/CategoriesFragment.java
@@ -2,16 +2,27 @@ package com.gunsnrocket.int20h.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gunsnrocket.int20h.R;
+import com.gunsnrocket.int20h.adapters.CategoryAdapter;
+import com.gunsnrocket.int20h.models.Category;
+
+import java.util.List;
 
 /**
  * Created by dnt on 2/27/16.
  */
 public class CategoriesFragment extends Fragment {
+
+    private List<Category> list;
+    private RecyclerView recyclerView;
+    private CategoryAdapter categoryAdapter;
+    private StaggeredGridLayoutManager layoutManager;
 
     public CategoriesFragment() {
     }
@@ -19,6 +30,13 @@ public class CategoriesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_categories, container, false);
+
+            recyclerView = (RecyclerView) rootView.findViewById(R.id.list_categories);
+            categoryAdapter = new CategoryAdapter(getContext(), list);
+            layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(categoryAdapter);
             return rootView;
     }
 
